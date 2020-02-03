816135428bce4beb0bbfbf362257041cd64012a1
David Long <longdn@dukes.jmu.edu>
2020-02-03 15:01:19 -0500

    severaldifferntfiles

---
+++ app.py
@@ -0,0 +1,4 @@
+name = input('What is Your name? ')
+print('Hi ' + name)
+color = input('What is your favorite color? ')
+print('Davids favorite color is ' + color)
---
+++ name_color.py
@@ -0,0 +1,4 @@
+name = input('What is Your name? ')
+color = input('What is your favorite color? ')
+print('Hi ' + name)
+print('Davids favorite color is ' + color)
