bb820e0dbf3da06d56a32118231e55f26af1f2df
David Long <longdn@dukes.jmu.edu>
2020-02-03 14:58:53 -0500

    birthyear.py

---
+++ birthyear.py
@@ -0,0 +1,10 @@
+birth_year = input('Birth year: ')
+
+age = 2020 - int(birth_year)
+
+print(age)
+
+weight_in_pounds = input('Weight (lbs): ')
+weight_in_Kilograms = .45*int(weight_in_pounds)
+print(weight_in_Kilograms)
+
